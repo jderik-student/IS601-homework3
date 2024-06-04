3b8c8d01fb4cb733b3959d30977424f3f36a3e94
jad553 <jad553@drexel.edu>
2024-06-04 14:25:34 -0400

    Added test coverage for the newly added the newly added methods

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,26 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+import pytest
+from calculator import add, subtract, multiply, divide
 
-def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+# pylint: disable=unnecessary-dunder-call, invalid-name
 
-def test_subtraction():
+def test_add():
     '''Test that addition function works '''    
-    assert subtract(2,2) == 0
+    assert add(6,2) == 8
+
+def test_subtract():
+    '''Test that subtraction function works '''    
+    assert subtract(6,2) == 4
+
+def test_multiply():
+    '''Test that multiplication function works '''    
+    assert multiply(6,2) == 12
+
+def test_division_nonZero_Divisor():
+    '''Test that division function works with a non-zero Divisor'''    
+    assert divide(6,2) == 3
+
+def test_division_Zero_Divisor():
+    '''Test that division function works '''    
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(6,0)

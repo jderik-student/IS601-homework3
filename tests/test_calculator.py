9c1b330803dd7c08db5e5dd0654f39a2d080fd42
jad553 <jad553@drexel.edu>
2024-06-04 15:04:16 -0400

    modified test_calculator.py to use Calculator's new static methods

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,26 +1,26 @@
 '''My Calculator Test'''
 import pytest
-from calculator import add, subtract, multiply, divide
+from calculator import Calculator
 
 # pylint: disable=unnecessary-dunder-call, invalid-name
 
 def test_add():
     '''Test that addition function works '''    
-    assert add(6,2) == 8
+    assert Calculator.add(6,2) == 8
 
 def test_subtract():
     '''Test that subtraction function works '''    
-    assert subtract(6,2) == 4
+    assert Calculator.subtract(6,2) == 4
 
 def test_multiply():
     '''Test that multiplication function works '''    
-    assert multiply(6,2) == 12
+    assert Calculator.multiply(6,2) == 12
 
 def test_division_nonZero_Divisor():
     '''Test that division function works with a non-zero Divisor'''    
-    assert divide(6,2) == 3
+    assert Calculator.divide(6,2) == 3
 
 def test_division_Zero_Divisor():
     '''Test that division function works '''    
     with pytest.raises(ValueError, match="Cannot divide by zero"):
-        divide(6,0)
+        Calculator.divide(6,0)

43a2d8a321f97d77b3708b5db94995cdd914cc3e
jad553 <jad553@drexel.edu>
2024-06-07 14:44:22 -0400

    added a factory method and created a string representation for Calculation

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,14 +1,52 @@
-'''
-    This class defines a calculation and stores its term and the operation to be calculated 
-'''
+# pylint: disable=unnecessary-dunder-call, invalid-name
+"""
+    Class representing a calculation which stores the two Decimal values to be used and the operation to be preformed 
+"""
+
+from decimal import Decimal
+from typing import Callable
 
 class Calculation:
-    def __init__(self, a, b, operation):
-        # Stores the two terms of the operation and the operation function
+    """
+        Class representing a calculation which stores the two Decimal values to be used and the operation to be preformed 
+    """
+
+    def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        """
+            Initalizes a Calculation
+
+            @param a: the first Decimal number to be used in the calculation
+            @param b: the second Decimal number to be used in the calculation
+            @param operation: the mathemtical operation to be performed (add, subtract, multiply, or divide)
+        """
         self.a = a
         self.b = b
         self.operation = operation
-    
-    def calculate(self):
-        # Calls the stored operation on its two terms
+
+    @staticmethod
+    def create(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
+        """
+            Factory method used to create instances of a Calculation
+
+            @param a: the first Decimal number to be used in the calculation
+            @param b: the second Decimal number to be used in the calculation
+            @param operation: the mathemtical operation to be performed (add, subtract, multiply, or divide)
+            @return: the newly created instance of a Calculation 
+        """
+        return Calculation(a, b, operation)
+
+    def calculate(self) -> Decimal:
+        """
+            Preforms the calclation based on the two terms and the operation function stored in the Calculation object
+
+            @return: the Decimal result of the preformed calculation
+        """
         return self.operation(self.a, self.b)
+
+    def __repr__(self):
+        """
+            String representation of a Calculation
+
+            @return: String representation of a Calculation
+        """
+        return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
